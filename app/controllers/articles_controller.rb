7dc254c35412cbd2c7577fcc8a171ac2d0f3dff6
竹内瑳彩 <tahiti.hawaii.29@gmail.com>
2023-05-22 17:21:39 +0900

    validation and flash action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,8 +14,9 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to article_path(@article)
+      redirect_to article_path(@article), notice: 'Saved!!'
     else
+      flash.now[:error] = 'Failed to save'
       render :new
     end
   end
