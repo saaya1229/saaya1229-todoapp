b4b395ac27f79942c77c35694b484b680f284d7f
竹内瑳彩 <tahiti.hawaii.29@gmail.com>
2023-05-31 16:02:03 +0900

    taskを保存できるように

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,5 +2,23 @@ class TasksController < ApplicationController
   def new
     article = Article.find(params[:article_id])
     @task = article.tasks.build
+    @task.user_id = current_user.id
+  end
+
+  def create
+    article = Article.find(params[:article_id])
+    @task = article.tasks.build(task_params)
+    @task.user_id = current_user.id
+    if @task.save
+      redirect_to article_path(article), notice: 'Your task is saved!! ;)'
+    else
+      flash.now[:error] = 'Faild to Saved your task:('
+      render :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content, :date)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -18,4 +18,6 @@
 class Task < ApplicationRecord
   belongs_to :article
   belongs_to :user
+  validates :title, presence: true
+  validates :content, presence: true
 end
