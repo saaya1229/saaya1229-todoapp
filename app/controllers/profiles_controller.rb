c1d12b3bf71dcbec6358521d0ca4b0a51c9f1a52
竹内瑳彩 <tahiti.hawaii.29@gmail.com>
2023-06-07 11:38:23 +0900

    update action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,4 +8,25 @@ class ProfilesController < ApplicationController
   def edit
     @profile = current_user.build_profile
   end
+
+  def update
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'Updated your profile!'
+    else
+      flash.now[:error] = 'Failed to update your profile:('
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :nickname,
+      :introducution,
+      :gender,
+      :birthday,
+      :dubscribed
+    )
+  end
 end
