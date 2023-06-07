94b2092f0f830032484aeb437f65958fd67ccd03
竹内瑳彩 <tahiti.hawaii.29@gmail.com>
2023-06-07 12:03:50 +0900

    プロフィールを更新

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,11 +6,12 @@ class ProfilesController < ApplicationController
   end
 
   def edit
-    @profile = current_user.build_profile
+    @profile = current_user.profile || current_user.build_profile
   end
 
   def update
-    @profile = current_user.build_profile(profile_params)
+    @profile = current_user.prepare_profile
+    @profile.assign_attributes(profile_params)
     if @profile.save
       redirect_to profile_path, notice: 'Updated your profile!'
     else
@@ -23,10 +24,10 @@ class ProfilesController < ApplicationController
   def profile_params
     params.require(:profile).permit(
       :nickname,
-      :introducution,
+      :introduction,
       :gender,
       :birthday,
-      :dubscribed
+      :subscribed
     )
   end
 end
